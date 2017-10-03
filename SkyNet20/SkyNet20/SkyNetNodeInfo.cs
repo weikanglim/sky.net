853cc81c96abda9143a3dcdee243b49e02b502bf
Wei Lim <weikanglim@gmail.com>
2017-10-02 23:02:45 -0500

    Add machine id parse/getting.

--- SkyNet20/SkyNet20/SkyNetNodeInfo.cs
+++ SkyNet20/SkyNet20/SkyNetNodeInfo.cs
@@ -20,5 +20,23 @@ namespace SkyNet20
         public String HostName { get; set; }
         public IPEndPoint EndPoint { get; set; }
         public Status Status { get; set; }
+
+        public static string GetMachineId(IPAddress address)
+        {
+            return $"{address.ToString()};{DateTime.UtcNow}";
+        }
+
+
+        public static Tuple<IPAddress, DateTime> ParseMachineId(string machineId)
+        {
+            string[] segments = machineId.Split(";");
+
+            if (segments.Length != 2)
+            {
+                throw new ArgumentException($"{machineId} is not a valid machine ID.");
+            }
+
+            return new Tuple<IPAddress, DateTime>(IPAddress.Parse(segments[0]), DateTime.Parse(segments[1]));
+        }
     }
 }
