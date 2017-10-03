e4a51d71db1572d0645577c9ca243fe4f57c253a
Wei Lim <weikanglim@gmail.com>
2017-10-02 23:02:59 -0500

    Stub commands and packet header.

---
+++ SkyNet20/SkyNet20/Network/Commands/GrepCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+
+namespace SkyNet20.Network.Commands
+{
+    [ProtoContract]
+    public class GrepCommand
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public string Query { get; set; }
+    }
+}
---
+++ SkyNet20/SkyNet20/Network/Commands/MembershipJoinCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkyNet20.Network.Commands
+{
+    public class MembershipJoinCommand
+    {
+    }
+}
