ea23d58cdb85e515921695015f99f93a77ce94a4
Wei Lim <weikanglim@gmail.com>
2017-11-25 11:02:24 -0600

    Add base Vertex class.

---
+++ SkyNet20/SkyNet20/Sava/Messaging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkyNet20.Sava
+{
+    public class Messaging
+    {
+        public static Action<string, string, byte[]> SendMessageDelegate;
+
+        public static void SendMessage(string sourceVertex, string destinationVertex, byte[] message)
+        {
+            SendMessageDelegate(sourceVertex, destinationVertex, message);
+        }
+    }
+}
---
+++ SkyNet20/SkyNet20/Sava/Vertex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+using System.IO;
+
+namespace SkyNet20.Sava
+{
+    [ProtoContract]
+    public abstract class Vertex<TVertexValue, TEdgeValue, TMessageValue>
+    {
+        [ProtoMember(1)]
+        public string VertexId { get; }
+        [ProtoMember(2)]
+        public TVertexValue Value { get; set; }
+        [ProtoMember(3)]
+        public List<Tuple<string, TEdgeValue>> OutEdges { get; }
+        public long Step { get; }
+
+        // Delegates
+        public delegate void SendMessageDelegate(string destinationVertex, TMessageValue message);
+
+        public abstract void Compute(List<TMessageValue> messages);
+
+        public void SendMessageTo(string destinationVertex, TMessageValue message)
+        {
+            byte[] serializedMessage;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize<TMessageValue>(ms, message);
+                serializedMessage = ms.GetBuffer();
+            }
+
+            Messaging.SendMessage(this.VertexId, destinationVertex, serializedMessage);
+        }
+
+        // !TODO: Implement
+        public abstract void VoteToHalt();
+        
+    }
+}
